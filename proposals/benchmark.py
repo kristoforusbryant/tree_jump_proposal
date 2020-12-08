c04113bd1089a3d4a987c957bbc6c283cea1cf96
kristoforusbryant <kristoforusbryant@gmail.com>
2020-12-08 16:42:08 +0800

    minor bug fixes in proposals

--- proposals/benchmark.py
+++ proposals/benchmark.py
@@ -1,17 +1,37 @@
+from utils.combinatorics import choose
+
 ### BENCHMARK 
 # prior
-def prior_s():
-    G = {i:[] for i in range(n)}
-    for i in range(n): 
-        for j in range(i):
-            if np.random.uniform() > 0.5:
-                G[i].append(j)
-                G[j].append(i)
-    for i in G.keys(): 
-        G[i].sort()
-    return G
-prior_e = lambda p: 0
-prior_r_e = lambda p0,p1: prior_e(p1) - prior_e(p0) 
+if PRIOR_TYPE=='uniform': 
+    def prior_s():
+        G = {i:[] for i in range(n)}
+        for i in range(n): 
+            for j in range(i):
+                if np.random.uniform() > 0.5:
+                    G[i].append(j)
+                    G[j].append(i)
+        for i in G.keys(): 
+            G[i].sort()
+        return G
+    prior_e = lambda p: 0
+    prior_r_e = lambda p0,p1: prior_e(p1) - prior_e(p0) 
+    
+elif PRIOR_TYPE=='size-based': 
+    def prior_s():
+        G = {i:[] for i in range(n)}
+        for i in range(n): 
+            for j in range(i):
+                if np.random.uniform() > 0.5:
+                    G[i].append(j)
+                    G[j].append(i)
+        for i in G.keys(): 
+            G[i].sort()
+        return G
+    def prior_e(p): 
+        m = int(n*(n-1)/2)
+        k = n_edges(p)
+        return( - np.log(m+1) - np.log(choose(m, k)) ) 
+    prior_r_e = lambda p0,p1: prior_e(p1) - prior_e(p0) 
 
 def prop_s(G): 
     i,j = random.choice(list(combinations(list(G.keys()), 2)))
@@ -25,7 +45,7 @@ def prop_s(G):
         G[j].append(i) 
         G[j].sort()
     return G 
-prop_e = lambda p: 0
+prop_e = lambda p0,p1: 0
 prop_r_e = lambda p0,p1: prop_e(p1) - prop_e(p0)
 
 # likelihood 
@@ -35,4 +55,5 @@ def lik_e(data, params):
     log_prob_approx = laplace_approx(params, delta + len(data), D_star) - laplace_approx(params, delta, D) # as log prob 
     return log_prob_approx
 
-lik_r_e = lambda data, p0, p1: lik_e(data, p1) - lik_e(data, p0)
+def lik_r_e(data, p0, p1): 
+    return lik_e(data, p1) - lik_e(data, p0)
--- proposals/cycle_basis.py
+++ proposals/cycle_basis.py
@@ -1,17 +1,36 @@
-import ..utils.cycle_basis_support
+from utils.cycle_basis_support import * 
+from utils.combinatorics import choose
 
+### CYCLE-BASIS
 # prior (given the basis)
-def prior_s(basis=basis): 
-    G = {i:[] for i in range(n)}
-    b_index = []
-    for i in range(len(basis)): 
-        if np.random.uniform() > 0.5:
-            G = basis_addition(G, basis[i])
-            b_index.append(i)
-    return (G, b_index) 
-prior_e = lambda p: 0
-prior_r_e = lambda p0,p1: prior_e(p1) - prior_e(p0) 
-
+if PRIOR_TYPE=='uniform': 
+    def prior_s(basis=basis): 
+        G = {i:[] for i in range(n)}
+        b_index = []
+        for i in range(len(basis)): 
+            if np.random.uniform() > 0.5:
+                G = basis_addition(G, basis[i])
+                b_index.append(i)
+        return (G, b_index) 
+    prior_e = lambda p: 0
+    prior_r_e = lambda p0,p1: prior_e(p1) - prior_e(p0) 
+    
+elif PRIOR_TYPE=='size-based': 
+    def prior_s(basis=basis): 
+        G = {i:[] for i in range(n)}
+        b_index = []
+        for i in range(len(basis)): 
+            if np.random.uniform() > 0.5:
+                G = basis_addition(G, basis[i])
+                b_index.append(i)
+        return (G, b_index) 
+    def prior_e(p): 
+        G, b_ind = p 
+        m = len(basis) 
+        k = len(b_ind)
+        return(- np.log(m+1) - np.log(choose(m, k)))
+    prior_r_e = lambda p0,p1: prior_e(p1) - prior_e(p0) 
+    
 def prop_s(params):
     G, b_i = params
     i = np.random.choice(len(basis))
@@ -21,15 +40,17 @@ def prop_s(params):
     else: 
         b_i_.append(i)
     return (basis_addition(G, basis[i]), b_i_)
-prop_e = lambda p: 0
-prop_r_e = lambda p0,p1: prop_e(p1) - prop_e(p0)
+prop_e = lambda p0, p1: 0
+prop_r_e = lambda p0,p1: prop_e(p0,p1) - prop_e(p1, p0)
 
 # likelihood 
 def lik_e(data, params): 
+    params = params[0]
     maxmin = lambda x: max(min(x,np.exp(700)),np.exp(-700))
     U = lambda x: np.transpose(x) @ x
     D_star = constrained_cov(params, D + U(data), np.eye(D.shape[0]))
     log_prob_approx = laplace_approx(params, delta + len(data), D_star) - laplace_approx(params, delta, D) # as log prob 
     return log_prob_approx
 
-lik_r_e = lambda data, p0, p1: lik_e(data, p1[0]) - lik_e(data, p0[0])
+def lik_r_e(data, p0, p1): 
+    return lik_e(data, p1) - lik_e(data, p0)
